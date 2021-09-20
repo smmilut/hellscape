693c4436fa7c01f0ab0a76bf627ca4e6c7aac405
Pil Smmilut <pil.smmilut@gmail.com>
2021-09-20 11:40:10 +0200

    refactor Sprite and Backdrop to avoid closure
    Was using closure for variable privacy, but :
    - private variables don't really need to be enforced
    - they are harder to clone
    - they have worse performance ?

--- www/graphics/backdrop.js
+++ www/graphics/backdrop.js
@@ -2,81 +2,75 @@ import * as Utils from "../utils.js";
 import * as Physics from "../game/physics.js";
 import * as Sprites from "../graphics/sprite.js";
 
+
 /*
 * A level background image
 */
-const newComponent_Backdrop = function newComponent_Backdrop(initOptions) {
-    // the object we are building
-    const obj_Backdrop = {
-        name: "backdrop",
-    };
-    let Backdrop_sheetImage;
-    let Backdrop_animationDirection, Backdrop_frameTime, Backdrop_frameIndex;
-
-    obj_Backdrop.init = async function Backdrop_init(levelGrid, pixelCanvas) {
-        obj_Backdrop.width = levelGrid.width;
-        obj_Backdrop.sheetSrc = initOptions.sheetSrc;
-        obj_Backdrop.sheetConfigUrl = initOptions.sheetConfigUrl;
+const Backdrop = {
+    name: "backdrop",
+    init: async function Backdrop_init(initOptions, levelGrid, pixelCanvas) {
+        this.width = levelGrid.width;
+        this.sheetSrc = initOptions.sheetSrc;
+        this.sheetConfigUrl = initOptions.sheetConfigUrl;
         const data = await Utils.Http.Request({
-            url: obj_Backdrop.sheetConfigUrl,
+            url: this.sheetConfigUrl,
         });
-        obj_Backdrop.sheetConfig = JSON.parse(data.responseText);
-        obj_Backdrop.sheetCellWidth = obj_Backdrop.sheetConfig.cellWidth;
-        obj_Backdrop.sheetCellHeight = obj_Backdrop.sheetConfig.cellHeight;
-        Backdrop_sheetImage = await Utils.File.ImageLoader.get(obj_Backdrop.sheetSrc);
+        this.sheetConfig = JSON.parse(data.responseText);
+        this.sheetCellWidth = this.sheetConfig.cellWidth;
+        this.sheetCellHeight = this.sheetConfig.cellHeight;
+        this.sheetImage = await Utils.File.ImageLoader.get(this.sheetSrc);
         // finally we have map data and a sprite sheet
-        return await generateBackdropAnimation(pixelCanvas);
-    };
-
-    async function generateBackdropAnimation(pixelCanvas) {
-        obj_Backdrop.frames = [];
-        obj_Backdrop.frameDuration = obj_Backdrop.sheetConfig.animation.frameDuration;
-        obj_Backdrop.animationLength = obj_Backdrop.sheetConfig.animation.length;
-        obj_Backdrop.animationType = obj_Backdrop.sheetConfig.animation.type;
-        Backdrop_frameTime = 0;
-        switch (obj_Backdrop.animationType) {
+        await this._generateBackdropAnimation(pixelCanvas);
+    },
+    _generateBackdropAnimation: async function Backdrop_generateBackdropAnimation(pixelCanvas) {
+        this.frames = [];
+        this.frameDuration = this.sheetConfig.animation.frameDuration;
+        this.animationLength = this.sheetConfig.animation.length;
+        this.animationType = this.sheetConfig.animation.type;
+        this.frameTime = 0;
+        switch (this.animationType) {
             case Sprites.ANIMATION_TYPE.FORWARD:
             case Sprites.ANIMATION_TYPE.PINGPONG:
-                Backdrop_frameIndex = 0;
-                Backdrop_animationDirection = Sprites.ANIMATION_DIRECTION.FORWARD;
+                this.frameIndex = 0;
+                this.animationDirection = Sprites.ANIMATION_DIRECTION.FORWARD;
             case Sprites.ANIMATION_TYPE.REVERSE:
                 // last frame
-                Backdrop_frameIndex = obj_Backdrop.animationLength - 1;
-                Backdrop_animationDirection = Sprites.ANIMATION_DIRECTION.BACKWARD;
+                this.frameIndex = this.animationLength - 1;
+                this.animationDirection = Sprites.ANIMATION_DIRECTION.BACKWARD;
                 break;
             case Sprites.ANIMATION_TYPE.NONE:
-                Backdrop_frameIndex = 0;
-                Backdrop_animationDirection = Sprites.ANIMATION_DIRECTION.STOPPED;
+                this.frameIndex = 0;
+                this.animationDirection = Sprites.ANIMATION_DIRECTION.STOPPED;
                 break;
         }
         const sourceY = 0;
         const framePromises = [];
-        for (let sourceX = 0, frameIndex = 0; frameIndex < obj_Backdrop.animationLength; sourceX += obj_Backdrop.sheetCellWidth, frameIndex++) {
+        for (let sourceX = 0, frameIndex = 0; frameIndex < this.animationLength; sourceX += this.sheetCellWidth, frameIndex++) {
             framePromises.push(
-                generatePatternImage(pixelCanvas, sourceX, sourceY).then(function addFrameImage(imageRepeat) {
-                    obj_Backdrop.frames[frameIndex] = imageRepeat;
-                })
+                this._generatePatternImage(pixelCanvas, sourceX, sourceY).then(function addFrameImage(imageRepeat) {
+                    this.frames[frameIndex] = imageRepeat;
+                }.bind(this))
             );
         }
         await Promise.all(framePromises);
-        obj_Backdrop.frameImage = obj_Backdrop.frames[Backdrop_frameIndex];
-    }
+        this.frameImage = this.frames[this.frameIndex];
+    },
     /*
     * Generate the background image from individual tile
     */
-    function generatePatternImage(pixelCanvas, sourceX, sourceY) {
+    _generatePatternImage: function Backdrop_generatePatternImage(pixelCanvas, sourceX, sourceY) {
         return new Promise(function promiseSingleBackdropImage(resolve, reject) {
             // create a new canvas for getting the base image of the backdrop
-            let [singleCanvas, singleContext] = pixelCanvas.newUnscaled(obj_Backdrop.sheetCellWidth, obj_Backdrop.sheetCellHeight);
-            singleContext.drawImage(Backdrop_sheetImage,
+            let [singleCanvas, singleContext] = pixelCanvas.newUnscaled(this.sheetCellWidth, this.sheetCellHeight);
+            singleContext.drawImage(this.sheetImage,
                 sourceX,
                 sourceY,
-                obj_Backdrop.sheetCellWidth,
-                obj_Backdrop.sheetCellHeight,
+                this.sheetCellWidth,
+                this.sheetCellHeight,
                 0,
                 0,
-                obj_Backdrop.sheetCellWidth,
-                obj_Backdrop.sheetCellHeight
+                this.sheetCellWidth,
+                this.sheetCellHeight
             );
             /// Export the single image to a separate image
             let imageSingleUri = singleCanvas.toDataURL();
@@ -89,14 +83,14 @@ const newComponent_Backdrop = function newComponent_Backdrop(initOptions) {
                 reject();
             });
             imageSingle.src = imageSingleUri;
-        }).then(function gotSingleBackdropImage(imageSingle) {
+        }.bind(this)).then(function gotSingleBackdropImage(imageSingle) {
             return new Promise(function promiseRepeatBackdropImage(resolve, reject) {
                 /// TODO : wait later to draw the repeating pattern only with the requested camera width and position
                 /// create a new canvas for compositing a repeating layer based on the single image
-                let [repeatCanvas, repeatContext] = pixelCanvas.newUnscaled(obj_Backdrop.width, obj_Backdrop.sheetCellHeight);
+                let [repeatCanvas, repeatContext] = pixelCanvas.newUnscaled(this.width, this.sheetCellHeight);
                 // easy debug // document.getElementById("hiddenloading").appendChild(repeatCanvas);
                 /// Draw
-                let pattern = repeatContext.createPattern(imageSingle, obj_Backdrop.sheetConfig.pattern.repetition);
+                let pattern = repeatContext.createPattern(imageSingle, this.sheetConfig.pattern.repetition);
                 repeatContext.rect(0, 0, repeatCanvas.width, repeatCanvas.height);
                 repeatContext.fillStyle = pattern;
                 repeatContext.fill();
@@ -111,54 +105,46 @@ const newComponent_Backdrop = function newComponent_Backdrop(initOptions) {
                     reject();
                 });
                 imageRepeat.src = imageRepeatUri;
-            })
-        });
-
-    };
-
-    obj_Backdrop.draw = function Backdrop_draw(context, position) {
-        context.drawImage(obj_Backdrop.frameImage, position.x, position.y);
-    };
-
-
+            }.bind(this))
+        }.bind(this));
+    },
+    draw: function Backdrop_draw(context, position) {
+        context.drawImage(this.frameImage, position.x, position.y);
+    },
     /*
     * update animation frame time, change frame, change animation direction if necessary
     */
-    obj_Backdrop.updateAnimation = function Backdrop_updateAnimation(timePassed) {
-        if (Backdrop_animationDirection != Sprites.ANIMATION_DIRECTION.STOPPED) {
-            Backdrop_frameTime += timePassed;
-            while (Backdrop_frameTime > obj_Backdrop.frameDuration) {
-                nextFrame();
-                obj_Backdrop.frameImage = obj_Backdrop.frames[Backdrop_frameIndex];
-                Backdrop_frameTime -= obj_Backdrop.frameDuration;
+    updateAnimation: function Backdrop_updateAnimation(timePassed) {
+        if (this.animationDirection != Sprites.ANIMATION_DIRECTION.STOPPED) {
+            this.frameTime += timePassed;
+            while (this.frameTime > this.frameDuration) {
+                this._nextFrame();
+                this.frameImage = this.frames[this.frameIndex];
+                this.frameTime -= this.frameDuration;
             }
         }
-    };
-
+    },
     /*
     * move animation to next frame, change animation direction if necessary
     */
-    function nextFrame() {
-        Backdrop_frameIndex += Backdrop_animationDirection;
-        if (Backdrop_frameIndex >= obj_Backdrop.animationLength || Backdrop_frameIndex < 0) {
-            switch (obj_Backdrop.animationType) {
+    _nextFrame: function Backdrop_nextFrame() {
+        this.frameIndex += this.animationDirection;
+        if (this.frameIndex >= this.animationLength || this.frameIndex < 0) {
+            switch (this.animationType) {
                 case Sprites.ANIMATION_TYPE.FORWARD:
                 case Sprites.ANIMATION_TYPE.BACKWARD:
-                    Backdrop_frameIndex = (Backdrop_frameIndex + obj_Backdrop.animationLength) % obj_Backdrop.animationLength;
+                    this.frameIndex = (this.frameIndex + this.animationLength) % this.animationLength;
                     break;
                 case Sprites.ANIMATION_TYPE.PINGPONG:
-                    Backdrop_animationDirection = -Backdrop_animationDirection;
-                    Backdrop_frameIndex += Backdrop_animationDirection;
+                    this.animationDirection = -this.animationDirection;
+                    this.frameIndex += this.animationDirection;
                     break;
                 default:
                     console.warn("unexpected case");
                     break;
             }
         }
-    };
-
-
-    return obj_Backdrop;
+    },
 };
 
 const layersSheetOptions = [
@@ -208,14 +194,18 @@ const layersSheetOptions = [
     },
 ];
 
-function spawnNewBackdrop(engine, layerOptions, levelGrid, pixelCanvas) {
-    const backdrop = newComponent_Backdrop(layerOptions.sheetOptions);
-    return backdrop.init(levelGrid, pixelCanvas)
-        .then(function backdropInitialized() {
-            return engine.spawn()
-                .addComponent(backdrop)
-                .addComponent(Physics.newComponent_Position(layerOptions.gamePosition));
-        })
+async function newComponent_Backdrop(initOptions, levelGrid, pixelCanvas) {
+    const backdrop = Object.create(Backdrop);
+    await backdrop.init(initOptions, levelGrid, pixelCanvas);
+    return backdrop;
+}
+
+
+async function spawnNewBackdrop(engine, layerOptions, levelGrid, pixelCanvas) {
+    const backdrop = await newComponent_Backdrop(layerOptions.sheetOptions, levelGrid, pixelCanvas);
+    return engine.spawn()
+        .addComponent(backdrop)
+        .addComponent(Physics.newComponent_Position(layerOptions.gamePosition));
 }
 
 const System_initBackdrops = {
--- www/graphics/sprite.js
+++ www/graphics/sprite.js
@@ -17,100 +17,86 @@ export const ANIMATION_TYPE = Object.freeze({
 /*
 * a Sprite with animation from a sprite sheet
 */
-export const newComponent_Sprite = async function newSprite(initOptions) {
-    const obj_Sprite = {
-        name: "sprite",
-    };
-    let Sprite_sheetImage;
-    let Sprite_pose, Sprite_frame, Sprite_frameTime, Sprite_animationDirection;
-    let Sprite_poseInfo, Sprite_frameInfo;
-    /*
-    * A map of :
-    *   {
-    *       poseName: {
-    *           frames: [
-    *               {  // frame info
-    *                   sourceX,
-    *                   sourceY
-    *               },
-    *           ],
-    *           pose,
-    *           animation,
-    *       }
-    *   }
-    */
-    const Sprite_sheetLayout = {};
-
-    /// TODO : BUG : initOptions never used, gotta simply this
-    obj_Sprite.init = async function Sprite_init(initOptions) {
-        obj_Sprite.sheetSrc = initOptions.sheetSrc;
-        obj_Sprite.sheetConfigUrl = initOptions.sheetConfigUrl;
+const Sprite = {
+    name: "sprite",
+    init: async function Sprite_init(initOptions) {
+        this.sheetSrc = initOptions.sheetSrc;
+        this.sheetConfigUrl = initOptions.sheetConfigUrl;
         const data = await Utils.Http.Request({
             url: initOptions.sheetConfigUrl,
         });
-        obj_Sprite.sheetConfig = JSON.parse(data.responseText);
-        //#region init sprite sheet
-        obj_Sprite.sheetCellWidth = obj_Sprite.sheetConfig.cellWidth;
-        obj_Sprite.sheetCellHeight = obj_Sprite.sheetConfig.cellHeight;
-        obj_Sprite.sheetLayout = obj_Sprite.sheetConfig.layout;
-        Sprite_sheetImage = await Utils.File.ImageLoader.get(obj_Sprite.sheetSrc);
-        return await parseSpriteSheet();
-        //#endregion
-    };
-
-    function parseSpriteSheet() {
+        this.sheetConfig = JSON.parse(data.responseText);
+        this.sheetCellWidth = this.sheetConfig.cellWidth;
+        this.sheetCellHeight = this.sheetConfig.cellHeight;
+        /*
+        * A map of :
+        *   {
+        *       poseName: {
+        *           frames: [
+        *               {  // frame info
+        *                   sourceX,
+        *                   sourceY
+        *               },
+        *           ],
+        *           pose,
+        *           animation,
+        *       }
+        *   }
+        */
+        this.sheetLayout = this.sheetConfig.layout;
+        this.sheetImage = await Utils.File.ImageLoader.get(this.sheetSrc);
+        await this._parseSpriteSheet();
+        this.setPose({ name: this.sheetConfig.defaultPose });
+    },
+    _parseSpriteSheet: function Sprite_parseSpriteSheet() {
         return new Promise(function parsingSpriteSheet(resolve, reject) {
             // Run through all cells in the sprite sheet to define separate animation frames
-            for (let sourceY = 0, poseIndex = 0; sourceY < Sprite_sheetImage.height; sourceY += obj_Sprite.sheetCellHeight, poseIndex++) {
+            for (let sourceY = 0, poseIndex = 0; sourceY < this.sheetImage.height; sourceY += this.sheetCellHeight, poseIndex++) {
                 // Y position in the sprite sheet is the animation pose
-                let options = obj_Sprite.sheetLayout[poseIndex];
+                let options = this.sheetLayout[poseIndex];
                 let animationOptions = options.animation;
                 let poseOptions = options.pose;
                 poseOptions.name = poseOptions.action + poseOptions.facing; // should already be like this, but instead of checking, I force, because it's only a convenience for inputting the options
-                Sprite_sheetLayout[poseOptions.name] = {
+                this.sheetLayout[poseOptions.name] = {
                     frames: [],
                     pose: poseOptions,
                     animation: animationOptions,
                 };
-                for (let sourceX = 0, frameIndex = 0; frameIndex < animationOptions.length; sourceX += obj_Sprite.sheetCellWidth, frameIndex++) {
+                for (let sourceX = 0, frameIndex = 0; frameIndex < animationOptions.length; sourceX += this.sheetCellWidth, frameIndex++) {
                     // X position in the sprite sheet is the animation frame for this pose
-                    Sprite_sheetLayout[poseOptions.name].frames[frameIndex] = {
+                    this.sheetLayout[poseOptions.name].frames[frameIndex] = {
                         sourceX: sourceX,
                         sourceY: sourceY,
                     };
                 }
             }
-            obj_Sprite.setPose({ name: initOptions.defaultPose });
             resolve();
-        });
-    }
-
+        }.bind(this));
+    },
     /*
     * From a requested drawing `position`, get where the sprite should be drawn.
     * Return the upper left corner of the sprite, when the `position` indicates the position of the bottom-middle.
     * */
-    function convertCenterPositionToSprite(position) {
-        let x = position.x - obj_Sprite.sheetCellWidth * 0.5;
+    _convertCenterPositionToSprite: function Sprite_convertCenterPositionToSprite(position) {
+        let x = position.x - this.sheetCellWidth * 0.5;
         // a bit off-center downwards, so that feet appear to walk on top of the lower map tile
-        let y = position.y - obj_Sprite.sheetCellHeight * 0.4;
+        let y = position.y - this.sheetCellHeight * 0.4;
         return { x: x, y: y };
-    }
-
-    obj_Sprite.draw = function Sprite_draw(context, position) {
-        let screenPosition = convertCenterPositionToSprite(position);
+    },
+    draw: function Sprite_draw(context, position) {
+        let screenPosition = this._convertCenterPositionToSprite(position);
         context.drawImage(
-            Sprite_sheetImage,
-            Sprite_frameInfo.sourceX,
-            Sprite_frameInfo.sourceY,
-            obj_Sprite.sheetCellWidth,
-            obj_Sprite.sheetCellHeight,
+            this.sheetImage,
+            this.frameInfo.sourceX,
+            this.frameInfo.sourceY,
+            this.sheetCellWidth,
+            this.sheetCellHeight,
             screenPosition.x,
             screenPosition.y,
-            obj_Sprite.sheetCellHeight,
-            obj_Sprite.sheetCellHeight
+            this.sheetCellHeight,
+            this.sheetCellHeight
         );
-    };
-
+    },
     /*
     * set animation pose
     *
@@ -125,75 +111,76 @@ export const newComponent_Sprite = async function newSprite(initOptions) {
     *       facing,
     *   }
     */
-    obj_Sprite.setPose = function Sprite_setPose(poseInfo) {
+    setPose: function Sprite_setPose(poseInfo) {
         let poseName = poseInfo.name;
         if (poseInfo.action && poseInfo.facing) {
             poseName = poseInfo.action + poseInfo.facing;
         }
-        if (poseName !== undefined && Sprite_pose != poseName) {
+        if (poseName !== undefined && this.pose != poseName) {
             // the pose changed
-            Sprite_pose = poseName;
+            this.pose = poseName;
             // reset animation
-            Sprite_frameTime = 0;
-            Sprite_poseInfo = Sprite_sheetLayout[Sprite_pose];
-            switch (Sprite_poseInfo.animation.type) {
+            this.frameTime = 0;
+            this.poseInfo = this.sheetLayout[this.pose];
+            switch (this.poseInfo.animation.type) {
                 case ANIMATION_TYPE.FORWARD:
                 case ANIMATION_TYPE.PINGPONG:
-                    Sprite_frame = 0;
-                    Sprite_animationDirection = ANIMATION_DIRECTION.FORWARD;
+                    this.frame = 0;
+                    this.animationDirection = ANIMATION_DIRECTION.FORWARD;
                 case ANIMATION_TYPE.REVERSE:
                     // last frame
-                    Sprite_frame = Sprite_poseInfo.animation.length - 1;
-                    Sprite_animationDirection = ANIMATION_DIRECTION.BACKWARD;
+                    this.frame = this.poseInfo.animation.length - 1;
+                    this.animationDirection = ANIMATION_DIRECTION.BACKWARD;
                     break;
                 case ANIMATION_TYPE.NONE:
-                    Sprite_frame = 0;
-                    Sprite_animationDirection = ANIMATION_DIRECTION.STOPPED;
+                    this.frame = 0;
+                    this.animationDirection = ANIMATION_DIRECTION.STOPPED;
                     break;
             }
-            Sprite_frameInfo = Sprite_poseInfo.frames[Sprite_frame];
+            this.frameInfo = this.poseInfo.frames[this.frame];
         }
-    };
-
+    },
     /*
     * update animation frame time, change frame, change animation direction if necessary
     */
-    obj_Sprite.updateAnimation = function Sprite_updateAnimation(timePassed) {
-        if (Sprite_animationDirection != ANIMATION_DIRECTION.STOPPED) {
-            Sprite_frameTime += timePassed;
-            while (Sprite_frameTime > Sprite_poseInfo.animation.frameDuration) {
-                nextFrame();
-                Sprite_frameTime -= Sprite_poseInfo.animation.frameDuration;
+    updateAnimation: function Sprite_updateAnimation(timePassed) {
+        if (this.animationDirection != ANIMATION_DIRECTION.STOPPED) {
+            this.frameTime += timePassed;
+            while (this.frameTime > this.poseInfo.animation.frameDuration) {
+                this._nextFrame();
+                this.frameTime -= this.poseInfo.animation.frameDuration;
             }
         }
-    };
-
+    },
     /*
     * move animation to next frame, change animation direction if necessary
     */
-    function nextFrame() {
-        const animationLength = Sprite_poseInfo.frames.length;
-        Sprite_frame += Sprite_animationDirection;
-        if (Sprite_frame >= animationLength || Sprite_frame < 0) {
-            switch (Sprite_poseInfo.animation.type) {
+    _nextFrame: function Sprite_nextFrame() {
+        const animationLength = this.poseInfo.frames.length;
+        this.frame += this.animationDirection;
+        if (this.frame >= animationLength || this.frame < 0) {
+            switch (this.poseInfo.animation.type) {
                 case ANIMATION_TYPE.FORWARD:
                 case ANIMATION_TYPE.BACKWARD:
-                    Sprite_frame = (Sprite_frame + animationLength) % animationLength;
+                    this.frame = (this.frame + animationLength) % animationLength;
                     break;
                 case ANIMATION_TYPE.PINGPONG:
-                    Sprite_animationDirection = -Sprite_animationDirection;
-                    Sprite_frame += Sprite_animationDirection;
+                    this.animationDirection = -this.animationDirection;
+                    this.frame += this.animationDirection;
                     break;
                 default:
                     console.warn("unexpected case");
                     break;
             }
         }
-        Sprite_frameInfo = Sprite_poseInfo.frames[Sprite_frame];
-    };
+        this.frameInfo = this.poseInfo.frames[this.frame];
+    },
+};
 
-    await obj_Sprite.init(initOptions);
-    return obj_Sprite;
+export async function newComponent_Sprite(initOptions) {
+    const sprite = Object.create(Sprite);
+    await sprite.init(initOptions);
+    return sprite;
 };
 
 const System_updateSpriteAnimation = {
